e798c988247728cf2f3be79e1fb17ffe46371b15
Chukwuka Honour <chukwukahonour@gmail.com>
2026-04-09 17:34:19 +0100

    chore: updated export file

--- packages/common/src/index.ts
+++ packages/common/src/index.ts
@@ -5,3 +5,9 @@ export type { Logger } from "winston";
 // Zod
 export * from "./env.js";
 export { z } from "zod";
+
+// Error
+export * from "./error/index.js";
+
+// Constants
+export { HTTPSTATUS, HttpStatusCodeType } from "./constants/http-status-code.js";
