3bd998f9adcd26fed7b9625195a303c7db9f0ce7
Chukwuka Honour <chukwukahonour@gmail.com>
2026-04-09 17:33:17 +0100

    feat: added error handler middleware for centralized error management

---
+++ packages/common/src/middlewares/error-handler.middleware.ts
@@ -0,0 +1,23 @@
+import { createLogger } from "../logger.js";
+import { ErrorRequestHandler } from "express";
+import { HTTPSTATUS } from "../constants/http-status-code.js";
+import { AppError } from "../error/app.error.js";
+
+const logger = createLogger({ name: "common" });
+
+export const errorHandlerMiddleware: ErrorRequestHandler = (err, req, res, next) => {
+  logger.error(err.message, { error: err });
+
+  if (err instanceof AppError) {
+    const response = err?.details
+      ? { message: err.message, errors: err.details.errors }
+      : { message: err.message };
+
+    return res.status(err.statusCode).json(response);
+  }
+
+  return res.status(HTTPSTATUS.INTERNAL_SERVER_ERROR).json({
+    message: "Internal server error",
+    error: err?.message || "An unknown error occurred",
+  });
+};
