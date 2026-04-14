626763421ed5da7ed008ab23c7423fa324e071ec
Chukwuka Honour <chukwukahonour@gmail.com>
2026-04-14 12:10:26 +0100

    feat: created a func to authenticate api key

--- services/auth/src/middlewares/authenticate-user.ts
+++ services/auth/src/middlewares/authenticate-user.ts
@@ -1,10 +1,12 @@
 import { env } from "@/config/env";
 import { Request, Response, NextFunction, type RequestHandler } from "express";
 import jwt from "jsonwebtoken";
+import { prisma } from "@repo/db";
 
 import { AuthenticatedUser } from "@/types/auth";
 
 import { AppError, HTTPSTATUS, UnauthorizedException } from "@repo/common";
+import { hashToken } from "@/utils";
 
 interface AccessTokenPayload {
   sub: string;
@@ -34,6 +36,50 @@ const validateAccessTokenPayload = (payload: AccessTokenPayload): AuthenticatedU
     email: payload.email,
   };
 };
+
+async function authenticateApiKey(key: string, req: Request, _res: Response) {
+  const ApikeyHash = hashToken(key);
+
+  const existingApiKey = await prisma.apiKey.findUnique({
+    where: {
+      keyHash: ApikeyHash,
+    },
+  });
+
+  if (!existingApiKey) {
+    throw new UnauthorizedException("Invalid API key");
+  }
+
+  if (existingApiKey.expiresAt && existingApiKey.expiresAt < new Date()) {
+    throw new UnauthorizedException("API key has expired");
+  }
+
+  const existingUser = await prisma.user.findFirst({
+    where: {
+      id: existingApiKey.userId,
+    },
+    select: {
+      id: true,
+      email: true,
+    },
+  });
+
+  if (!existingUser) {
+    throw new UnauthorizedException("User not found");
+  }
+
+  // Update lastUsedAt (fire-and-forget — don't slow down the request)
+  await prisma.apiKey.update({
+    where: {
+      id: existingApiKey.id,
+    },
+    data: {
+      lastUsedAt: new Date(),
+    },
+  });
+
+  req.user = { id: existingUser.id, email: existingUser.email };
+}
 
 export const authenticateUser: RequestHandler = (
   req: Request,
