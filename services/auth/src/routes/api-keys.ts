ba4fc1e1a04991aa27e18cc254ca0088cff7d415
Chukwuka Honour <chukwukahonour@gmail.com>
2026-04-14 12:48:56 +0100

    feat: added route for creating api key

---
+++ services/auth/src/routes/api-keys.ts
@@ -0,0 +1,14 @@
+import { createApiKeyHandler } from "@/controllers/api-keys.controller";
+import { authenticateUser } from "@/middlewares/authenticate-user";
+import { createApiKeySchema } from "@/validators/api-keys";
+import { validateRequest } from "@repo/common";
+import { Router } from "express";
+
+export const apiKeyRoutes: Router = Router();
+
+apiKeyRoutes.post(
+  "/",
+  validateRequest({ body: createApiKeySchema }),
+  authenticateUser,
+  createApiKeyHandler,
+);
--- services/auth/src/routes/index.ts
+++ services/auth/src/routes/index.ts
@@ -1,6 +1,8 @@
 import { Router } from "express";
 import { authRoutes } from "@/routes/auth";
+import { apiKeyRoutes } from "@/routes/api-keys";
 
 export const registerRoutes = (app: Router) => {
   app.use("/auth", authRoutes);
+  app.use("/api-keys", apiKeyRoutes);
 };
