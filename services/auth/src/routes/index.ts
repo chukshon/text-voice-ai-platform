e3c63902a7b3a3e9e574a620a5c306d0d674ed64
Chukwuka Honour <chukwukahonour@gmail.com>
2026-04-15 14:35:57 +0100

    chore: renamed routes

--- services/auth/src/routes/index.ts
+++ services/auth/src/routes/index.ts
@@ -1,6 +1,6 @@
 import { Router } from "express";
-import { authRoutes } from "@/routes/auth";
-import { apiKeyRoutes } from "@/routes/api-keys";
+import { authRoutes } from "@/routes/auth.routes";
+import { apiKeyRoutes } from "@/routes/api-keys.routes";
 
 export const registerRoutes = (app: Router) => {
   app.use("/auth", authRoutes);
