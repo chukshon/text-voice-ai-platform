b11511fe1cf85c66a01478ebc99d3594f1837ed0
Chukwuka Honour <chukwukahonour@gmail.com>
2026-04-15 15:36:08 +0100

    chore: renamed voice root route

--- services/voice/src/routes/index.ts
+++ services/voice/src/routes/index.ts
@@ -2,5 +2,5 @@ import { Router } from "express";
 import { voiceRoutes } from "@/routes/voice.routes";
 
 export const registerRoutes = (app: Router) => {
-  app.use("/voice", voiceRoutes);
+  app.use("/voices", voiceRoutes);
 };
