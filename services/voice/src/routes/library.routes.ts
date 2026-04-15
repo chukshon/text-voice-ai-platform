d7113195549ac2741edab19987ea0c73c74c95eb
Chukwuka Honour <chukwukahonour@gmail.com>
2026-04-15 22:27:17 +0100

    feat: added get voice by id route

--- services/voice/src/routes/library.routes.ts
+++ services/voice/src/routes/library.routes.ts
@@ -1,8 +1,11 @@
-import { listAllVoicesHandler } from "@/controllers/library.controller";
+import { getVoiceByIdHandler, listAllVoicesHandler } from "@/controllers/library.controller";
 import { Router } from "express";
 import { validateRequest } from "@repo/common";
 import { listAllVoicesQuerySchema } from "@/validators/library";
+import { paramsIdSchema } from "@/validators/shared";
 
 export const libraryRoutes: Router = Router();
 
 libraryRoutes.get("/", validateRequest({ query: listAllVoicesQuerySchema }), listAllVoicesHandler);
+
+libraryRoutes.get("/:id", validateRequest({ params: paramsIdSchema }), getVoiceByIdHandler);
