369059c94c81468c3031950ef4b399b804c1fbeb
Chukwuka Honour <chukwukahonour@gmail.com>
2026-04-16 17:44:10 +0100

    feat: add route to delete voice samples by ID

--- services/voice/src/routes/voice-samples.routes.ts
+++ services/voice/src/routes/voice-samples.routes.ts
@@ -4,6 +4,7 @@ import {
   createVoiceSampleHandler,
   getVoiceSampleByIdHandler,
   listAllUserVoiceSamplesHandler,
+  deleteVoiceSampleByIdHandler,
 } from "@/controllers/voice-samples.controller";
 import { authenticateUser } from "@/middlewares/authenticate-user";
 
@@ -12,3 +13,4 @@ export const voiceSampleRoutes: Router = Router();
 voiceSampleRoutes.post("/", authenticateUser, upload.single("file"), createVoiceSampleHandler);
 voiceSampleRoutes.get("/", authenticateUser, listAllUserVoiceSamplesHandler);
 voiceSampleRoutes.get("/:sampleId", authenticateUser, getVoiceSampleByIdHandler);
+voiceSampleRoutes.delete("/:sampleId", authenticateUser, deleteVoiceSampleByIdHandler);
