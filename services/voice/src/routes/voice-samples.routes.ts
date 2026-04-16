474153cd4c5d6b4f062c0ea181917a39d4a2280a
Chukwuka Honour <chukwukahonour@gmail.com>
2026-04-16 17:36:32 +0100

    feat: add route to list all user voice samples

--- services/voice/src/routes/voice-samples.routes.ts
+++ services/voice/src/routes/voice-samples.routes.ts
@@ -1,8 +1,12 @@
 import { Router } from "express";
 import { upload } from "@/lib/multer";
-import { createVoiceSampleHandler } from "@/controllers/voice-samples.controller";
+import {
+  createVoiceSampleHandler,
+  listAllUserVoiceSamplesHandler,
+} from "@/controllers/voice-samples.controller";
 import { authenticateUser } from "@/middlewares/authenticate-user";
 
 export const voiceSampleRoutes: Router = Router();
 
 voiceSampleRoutes.post("/", authenticateUser, upload.single("file"), createVoiceSampleHandler);
+voiceSampleRoutes.get("/", authenticateUser, listAllUserVoiceSamplesHandler);
