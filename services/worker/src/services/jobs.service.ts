5936ac06a210af9ec63d57ea5773864dd95e1d91
Chukwuka Honour <chukwukahonour@gmail.com>
2026-04-17 17:24:46 +0100

    feat(worker): implement createJobService

---
+++ services/worker/src/services/jobs.service.ts
@@ -0,0 +1,45 @@
+import { prisma } from "@repo/db";
+import { logger } from "@/utils/logger";
+import { NotFoundException } from "@repo/common";
+import { CreateJobInputT } from "@/validators/job.validator";
+import { publishJob } from "@/lib/queue";
+
+export const createJobService = async (payload: CreateJobInputT, userId: string) => {
+  const voice = await prisma.voice.findFirst({
+    where: {
+      id: payload.voiceId,
+      AND: {
+        OR: [{ isPublic: true }, { userId }],
+      },
+    },
+  });
+
+  if (!voice) {
+    logger.error("Voice not found", { voiceId: payload.voiceId });
+    throw new NotFoundException("Voice not found");
+  }
+  const createdJob = await prisma.voiceJob.create({
+    data: {
+      ...payload,
+      userId,
+      voiceId: payload.voiceId,
+    },
+    select: {
+      id: true,
+      status: true,
+      type: true,
+      voiceId: true,
+      createdAt: true,
+    },
+  });
+
+  logger.info("Job created successfully", { jobId: createdJob.id });
+
+  await publishJob({
+    jobId: createdJob.id,
+    voiceId: createdJob.voiceId,
+    text: payload.text,
+    outputFormat: payload.outputFormat,
+  });
+  return createdJob;
+};
