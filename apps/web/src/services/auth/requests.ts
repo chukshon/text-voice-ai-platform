398f906a1def5fd1213f2826beaaea66f7b37312
Chukwuka Honour <chukwukahonour@gmail.com>
2026-04-24 11:52:02 +0100

    feat(auth): add getLoggedInUserRequest and update types for user and refresh token responses

--- apps/web/src/services/auth/requests.ts
+++ apps/web/src/services/auth/requests.ts
@@ -2,8 +2,11 @@ import api from "@/config/axios";
 import {
   LoginPayloadT,
   LoginResponseT,
+  RefreshTokenPayloadT,
   RegisterPayloadT,
   RegisterResponseT,
+  GetLoggedInUserResponseT,
+  RefreshTokenResponseT,
 } from "@/services/auth/types";
 
 export const loginRequest = async (payload: LoginPayloadT): Promise<LoginResponseT> => {
@@ -13,5 +16,10 @@ export const loginRequest = async (payload: LoginPayloadT): Promise<LoginRespons
 
 export const registerRequest = async (payload: RegisterPayloadT): Promise<RegisterResponseT> => {
   const { data } = await api.post<RegisterResponseT>("auth/register", payload);
+  return data;
+};
+
+export const getLoggedInUserRequest = async (): Promise<GetLoggedInUserResponseT> => {
+  const { data } = await api.get<GetLoggedInUserResponseT>("auth/me");
   return data;
 };
--- apps/web/src/services/auth/types.ts
+++ apps/web/src/services/auth/types.ts
@@ -1,5 +1,11 @@
 import { ApiSuccessResponseT } from "@/types/api";
 
+export interface UserT {
+  id: string;
+  name: string;
+  email: string;
+  createdAt: string;
+}
 export interface LoginPayloadT {
   email: string;
   password: string;
@@ -16,28 +22,26 @@ export interface RefreshTokenPayloadT {
 }
 
 export type LoginResponseT = ApiSuccessResponseT<{
-  user: {
-    id: string;
-    name: string;
-    email: string;
-    createdAt: string;
-  };
+  user: UserT;
   accessToken: string;
   refreshToken: string;
   refreshTokenExpiresAt: Date;
 }>;
 
 export type RegisterResponseT = ApiSuccessResponseT<{
-  user: {
-    id: string;
-    name: string;
-    email: string;
-    createdAt: string;
-  };
+  user: UserT;
   accessToken: string;
   refreshToken: string;
   refreshTokenExpiresAt: Date;
 }>;
+
+export type RefreshTokenResponseT = ApiSuccessResponseT<{
+  accessToken: string;
+  refreshToken: string;
+  refreshTokenExpiresAt: Date;
+}>;
+
+export type GetLoggedInUserResponseT = ApiSuccessResponseT<UserT>;
 
 export const TOKEN_KEYS = {
   ACCESS_TOKEN: "access_token",
