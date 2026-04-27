8568cd28efd40ef78dd0f0223632322ca60f3d33
Chukwuka Honour <chukwukahonour@gmail.com>
2026-04-27 14:31:11 +0100

    refactor(voice): rename query parameter in useGetLibrary for clarity and update queryKey structure

--- apps/web/src/services/voices/queries.ts
+++ apps/web/src/services/voices/queries.ts
@@ -3,9 +3,9 @@ import { getLibraryRequest } from "@/services/voices/requests";
 import { GetLibraryResponseT } from "@/services/voices/types";
 import { GetLibraryQueryT } from "@/schema/voices.schema";
 
-export const useGetLibrary = (query: GetLibraryQueryT) => {
+export const useGetLibrary = (getLibraryQuery: GetLibraryQueryT) => {
   return useQuery<GetLibraryResponseT>({
-    queryKey: ["library"],
-    queryFn: () => getLibraryRequest(query),
+    queryKey: ["library", getLibraryQuery],
+    queryFn: () => getLibraryRequest(getLibraryQuery),
   });
 };
