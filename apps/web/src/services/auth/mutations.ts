f39a73a39b9b81b28a337a0586419f82f397248a
Chukwuka Honour <chukwukahonour@gmail.com>
2026-04-27 08:35:46 +0100

    feat(auth): added user registration mutation

--- apps/web/src/services/auth/mutations.ts
+++ apps/web/src/services/auth/mutations.ts
@@ -1,12 +1,27 @@
 import { useMutation } from "@tanstack/react-query";
-import { loginRequest } from "@/services/auth/requests";
-import { LoginPayloadT, LoginResponseT } from "@/services/auth/types";
+import { loginRequest, registerRequest } from "@/services/auth/requests";
+import {
+  LoginPayloadT,
+  LoginResponseT,
+  RegisterPayloadT,
+  RegisterResponseT,
+} from "@/services/auth/types";
 import { ApiErrorResponseT } from "@/types/api";
 import { toast } from "react-hot-toast";
 
 export const useLoginMutation = () => {
   return useMutation<LoginResponseT, ApiErrorResponseT, LoginPayloadT>({
     mutationFn: loginRequest,
+    onSuccess: (data) => {},
+    onError: (error) => {
+      toast.error(error.message);
+    },
+  });
+};
+
+export const useRegisterMutation = () => {
+  return useMutation<RegisterResponseT, ApiErrorResponseT, RegisterPayloadT>({
+    mutationFn: registerRequest,
     onSuccess: (data) => {},
     onError: (error) => {
       toast.error(error.message);
