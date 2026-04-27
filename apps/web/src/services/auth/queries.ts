7ace1479794bacbecb471603b11ab7bc7da43714
Chukwuka Honour <chukwukahonour@gmail.com>
2026-04-27 09:47:48 +0100

    minor edit

--- apps/web/src/services/auth/queries.ts
+++ apps/web/src/services/auth/queries.ts
@@ -1,8 +1,9 @@
 import { useQuery } from "@tanstack/react-query";
 import { getLoggedInUserRequest } from "@/services/auth/requests";
+import { GetLoggedInUserResponseT } from "./types";
 
 export const useGetLoggedInUserQuery = (isAuthenticated: boolean) => {
-  return useQuery({
+  return useQuery<GetLoggedInUserResponseT>({
     queryKey: ["loggedInUser"],
     queryFn: () => getLoggedInUserRequest(),
     enabled: isAuthenticated,
