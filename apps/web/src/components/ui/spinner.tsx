b90f53bd0dca19a1b1aca1e2d1ae67f12f0ab802
Chukwuka Honour <chukwukahonour@gmail.com>
2026-04-27 09:20:26 +0100

    refactoe(web): updated Spinner component with size variants and type options

--- apps/web/src/components/ui/spinner.tsx
+++ apps/web/src/components/ui/spinner.tsx
@@ -1,10 +1,38 @@
-import { cn } from "@/lib/utils"
-import { Loader2Icon } from "lucide-react"
+import { cn } from "@/lib/utils";
+import { cva } from "class-variance-authority";
+import { Loader, LoaderCircle } from "lucide-react";
 
-function Spinner({ className, ...props }: React.ComponentProps<"svg">) {
+type SpinnerContentProps = {
+  size?: "small" | "medium" | "large";
+  children?: React.ReactNode;
+  className?: string;
+  icon?: string;
+  type?: "loader" | "loaderCircle";
+};
+
+function Spinner({ className, type = "loader", size = "medium", children }: SpinnerContentProps) {
+  const loaderVariants = cva("animate-spin text-primary", {
+    variants: {
+      size: {
+        small: "size-6",
+        medium: "size-8",
+        large: "size-12",
+      },
+    },
+    defaultVariants: {
+      size: "medium",
+    },
+  });
   return (
-    <Loader2Icon role="status" aria-label="Loading" className={cn("size-4 animate-spin", className)} {...props} />
-  )
+    <span>
+      {type === "loader" ? (
+        <Loader className={cn(loaderVariants({ size }), className, "text-lemon-base")} />
+      ) : (
+        <LoaderCircle className={cn(loaderVariants({ size }), className, "text-lemon-base")} />
+      )}
+      {children}
+    </span>
+  );
 }
 
-export { Spinner }
+export { Spinner };
