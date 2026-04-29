d0187f12de398f15bee9a3a43015b4578f5ba3d1
Chukwuka Honour <chukwukahonour@gmail.com>
2026-04-29 12:34:27 +0100

    feat(voice): invalidate voices query on successful voice creation

--- apps/web/src/services/voices/mutations.ts
+++ apps/web/src/services/voices/mutations.ts
@@ -2,13 +2,17 @@ import { CreateVoiceResponseT } from "@/services/voices/types";
 import { CreateVoicePayloadT } from "@/schema/voices.schema";
 import { createVoiceRequest } from "@/services/voices/requests";
 import { ApiErrorResponseT } from "@/types/api";
-import { useMutation } from "@tanstack/react-query";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+
 import { toast } from "react-hot-toast";
 
 export const useCreateVoiceMutation = () => {
+  const queryClient = useQueryClient();
   return useMutation<CreateVoiceResponseT, ApiErrorResponseT, CreateVoicePayloadT>({
     mutationFn: createVoiceRequest,
-    onSuccess: (data) => {},
+    onSuccess: (data) => {
+      queryClient.invalidateQueries({ queryKey: ["get-voices"] });
+    },
     onError: (error) => {
       toast.error(error.message);
     },
