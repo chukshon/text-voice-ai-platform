efa9858054a7e35b6487912d5d185b5b0b1a9208
Chukwuka Honour <chukwukahonour@gmail.com>
2026-04-29 11:51:49 +0100

    feat(web): add useGetVoices hook to fetch voices

--- apps/web/src/services/voices/queries.ts
+++ apps/web/src/services/voices/queries.ts
@@ -1,11 +1,18 @@
 import { useQuery } from "@tanstack/react-query";
-import { getLibraryRequest } from "@/services/voices/requests";
-import { GetLibraryResponseT } from "@/services/voices/types";
+import { getLibraryRequest, getVoicesRequest } from "@/services/voices/requests";
+import { GetLibraryResponseT, GetVoicesResponseT } from "@/services/voices/types";
 import { GetLibraryQueryT } from "@/schema/voices.schema";
 
 export const useGetLibrary = (getLibraryQuery: GetLibraryQueryT) => {
   return useQuery<GetLibraryResponseT>({
     queryKey: ["library", getLibraryQuery],
     queryFn: () => getLibraryRequest(getLibraryQuery),
+  });
+};
+
+export const useGetVoices = () => {
+  return useQuery<GetVoicesResponseT>({
+    queryKey: ["get-voices"],
+    queryFn: () => getVoicesRequest(),
   });
 };
