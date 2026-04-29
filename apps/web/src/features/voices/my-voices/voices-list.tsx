b97641fcd0da127c64055b109e3500e2a2e7e740
Chukwuka Honour <chukwukahonour@gmail.com>
2026-04-29 12:13:11 +0100

    feat(web): implement voices list component to display user voices with loading and empty states

---
+++ apps/web/src/features/voices/my-voices/voices-list.tsx
@@ -0,0 +1,35 @@
+import { VoiceT } from "@/services/voices/types";
+import LoadingState from "@/features/voices/loading-state";
+import EmptyState from "@/features/voices/my-voices/empty-state";
+import VoiceCard from "./voice-card";
+
+interface VoicesListProps {
+  voices: VoiceT[];
+  isLoading: boolean;
+  handleCreateVoice: () => void;
+}
+const VoicesList = ({ voices, isLoading, handleCreateVoice }: VoicesListProps) => {
+  if (isLoading) {
+    return <LoadingState />;
+  }
+  if (isLoading || voices?.length === 0) {
+    return <EmptyState handleCreateVoice={handleCreateVoice} />;
+  }
+  return (
+    <div className="space-y-2">
+      {voices?.map((voice) => (
+        <VoiceCard
+          key={voice.id}
+          voiceId={voice.id}
+          voiceName={voice.name}
+          voiceDescription={voice.description}
+          voiceCategory={voice.category}
+          voiceLanguage={voice.language}
+          voiceIsPublic={voice.isPublic}
+        />
+      ))}
+    </div>
+  );
+};
+
+export default VoicesList;
